8e6b772a123d9871a3d5f4eed493f70935e7b002
MarselSidikov <sidikov.marsel@gmail.com>
2017-10-14 16:34:10 +0300

    add task3

---
+++ ClassWorks/Task3/src/ru/itis/Main.java
@@ -0,0 +1,15 @@
+package ru.itis;
+
+public class Main {
+
+    public static void showReverse(int array[]) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+	    int a[] = {11, 12, 13, 14, 15};
+	    showReverse(a);
+    }
+}
