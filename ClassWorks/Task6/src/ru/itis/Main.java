abe4805f0642100af6b42ba2f71e54a8b3b5e224
MarselSidikov <sidikov.marsel@gmail.com>
2017-10-18 20:20:15 +0300

    add task6

---
+++ ClassWorks/Task6/src/ru/itis/Main.java
@@ -0,0 +1,58 @@
+package ru.itis;
+
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isLetter(char c) {
+        if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isDigit(char c) {
+        return (c >= '0' && c <= '9');
+    }
+
+    // '1' -> 1
+    // '8' -> 8
+    public static int convertToIntFromDigit(char c) {
+        // c = '1', код - 49
+        // '1' - '0' = 49 - 48 = 1
+        // c = '5', код - 53
+        // '5' - '0' = 53 - 48 = 5
+        return c - '0';
+    }
+
+    // ['2','3','1','6'] -> 2316
+    public static int parseNumber(char digits[]) {
+        int result = 0;
+        int pow = 1;
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            result = result + convertToIntFromDigit(digits[i]) * pow;
+            pow = pow * 10;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        // создаем scanner, который будет считывать с консоли
+        Scanner scanner = new Scanner(System.in);
+
+        // просим пользователя ввести длину числа, которое он введет (сколько в нем цифр?)
+        int lengthNumber = scanner.nextInt();
+        // создаем массив символов для этого числа
+        char number[] = new char[lengthNumber];
+        // посимвольно считываем цифры числа
+        for (int i = 0; i < number.length; i++) {
+            // scanner.next() - возвращает строку
+            // charAt(0) - первый строки
+            number[i] = scanner.next().charAt(0);
+        }
+        // в итоге в массиве number будут цифры числа, введенных пользователем
+
+        int x = parseNumber(number);
+        System.out.println(x);
+    }
+}
--- Lessons/15. Characters/src/ru/itis/Main.java
+++ Lessons/15. Characters/src/ru/itis/Main.java
@@ -1,34 +1,6 @@
 package ru.itis;
 
 public class Main {
-    public static boolean isLetter(char c) {
-        if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z') {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static boolean isDigit(char c) {
-        return (c >= '0' && c <= '9');
-    }
-
-    // '1' -> 1
-    // '8' -> 8
-    public static int convertToIntFromDigit(char c) {
-        // c = '1', код - 49
-        // '1' - '0' = 49 - 48 = 1
-        // c = '5', код - 53
-        // '5' - '0' = 53 - 48 = 5
-        return c - '0';
-    }
-
-    // ['2','3','1','6'] -> 2316
-    public static int parseNumber(char digits[]) {
-        return -1;
-    }
-
-
     public static void main(String[] args) {
 	    char letterA = 'A';
 	    // результат операции char + int -> int
